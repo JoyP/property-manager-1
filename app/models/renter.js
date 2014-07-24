ba59669e7315f7adcb284a0dd25cd7d03af7706a
Chyld Medford <chyld.medford@gmail.com>
2014-07-24 10:50:05 -0500

    renter model

--- app/models/renter.js
+++ app/models/renter.js
@@ -5,9 +5,43 @@ function Renter(name, age, gender, profession){
   this.age = parseInt(age);
   this.gender = gender;
   this.profession = profession;
-  this.isEvicted = false;
-  this.cash = Math.floor(Math.random() * 4901) + 100;
+  this._isEvicted = false;
+  this._cash = Math.floor(Math.random() * 4901) + 100;
 }
+
+Renter.prototype.work = function(){
+  switch(this.profession){
+    case 'Movie Star':
+      this._cash += Math.floor(Math.random() * 7001) + 3000;
+      break;
+    case 'Coder':
+      this._cash += Math.floor(Math.random() * 6001) + 1000;
+      break;
+    case 'Waiter':
+      this._cash += Math.floor(Math.random() * 201) + 50;
+      break;
+    case 'Social Worker':
+      this._cash += Math.floor(Math.random() * 601) + 150;
+  }
+};
+
+Renter.prototype.payRent = function(amount){
+  if(this.isEvicted){return;}
+
+  amount = parseInt(amount);
+  this._isEvicted = this._cash < amount;
+
+  if(!this._isEvicted){
+    this._cash -= amount;
+  }
+};
+
+Renter.prototype.party = function(){
+  if(this.isEvicted){return;}
+
+  var volume = Math.floor(Math.random() * 10) + 1;
+  this.isEvicted = volume > 8;
+};
 
 module.exports = Renter;
 
--- test/unit/renter.js
+++ test/unit/renter.js
@@ -15,9 +15,71 @@ describe('Renter', function(){
       expect(bob.name).to.equal('Bob Smith');
       expect(bob.age).to.equal(25);
       expect(bob.gender).to.equal('Male');
-      expect(bob.isEvicted).to.be.false;
+      expect(bob._isEvicted).to.be.false;
       expect(bob.profession).to.equal('Coder');
-      expect(bob.cash).to.be.within(100, 5000);
+      expect(bob._cash).to.be.within(100, 5000);
+    });
+  });
+
+  describe('#work', function(){
+    it('should increase cash for the renter', function(){
+      var bob = new Renter('Bob Smith', '25', 'Male', 'Coder');
+      bob._cash = 0;
+      bob.work();
+
+      expect(bob._cash).to.be.within(1000, 7000);
+    });
+  });
+
+  describe('#payRent', function(){
+    it('should pay the rent', function(){
+      var bob = new Renter('Bob Smith', '25', 'Male', 'Coder');
+      bob._cash = 1000;
+      bob.payRent('750');
+
+      expect(bob._cash).to.equal(250);
+      expect(bob._isEvicted).to.be.false;
+    });
+
+    it('should NOT pay the rent - not enough money', function(){
+      var bob = new Renter('Bob Smith', '25', 'Male', 'Coder');
+      bob._cash = 50;
+      bob.payRent('750');
+
+      expect(bob._cash).to.equal(50);
+      expect(bob._isEvicted).to.be.true;
+    });
+  });
+
+  describe('#party', function(){
+    it('should cause no disturbance - party on', function(){
+      var bob;
+
+      while(true){
+        bob = new Renter('Bob Smith', '25', 'Male', 'Coder');
+        bob.party();
+
+        if(!bob.isEvicted){
+          break;
+        }
+      }
+
+      expect(bob.isEvicted).to.be.false;
+    });
+
+    it('should cause police to be called', function(){
+      var bob;
+
+      while(true){
+        bob = new Renter('Bob Smith', '25', 'Male', 'Coder');
+        bob.party();
+
+        if(bob.isEvicted){
+          break;
+        }
+      }
+
+      expect(bob.isEvicted).to.be.true;
     });
   });
 });
